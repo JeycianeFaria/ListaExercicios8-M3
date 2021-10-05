e51bf27e3197143d23fa3d8df4fca0ffd75a4503
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-10-05 11:37:07 -0300

    Modelar a classe imovél

---
+++ src/br/com/zup/Imovel.java
@@ -0,0 +1,65 @@
+package br.com.zup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Imovel {
+
+    private  String tituloImovel;
+    private String endereco;
+    private double valorAluguel;
+    private Funcionario funcionarioResponsavel;
+    private List<Morador> moradores = new ArrayList<>();
+
+
+    public Imovel() {
+    }
+
+    public Imovel(String tituloImovel, String endereco, double valorAluguel) {
+        this.tituloImovel = tituloImovel;
+        this.endereco = endereco;
+        this.valorAluguel = valorAluguel;
+    }
+
+
+    public String getTituloImovel() {
+        return tituloImovel;
+    }
+
+    public void setTituloImovel(String tituloImovel) {
+        this.tituloImovel = tituloImovel;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public double getValorAluguel() {
+        return valorAluguel;
+    }
+
+    public void setValorAluguel(double valorAluguel) {
+        this.valorAluguel = valorAluguel;
+    }
+
+    public Funcionario getFuncionarioResponsavel() {
+        return funcionarioResponsavel;
+    }
+
+    public void setFuncionarioResponsavel(Funcionario funcionarioResponsavel) {
+        this.funcionarioResponsavel = funcionarioResponsavel;
+    }
+
+    public List<Morador> getMoradores() {
+        return moradores;
+    }
+
+    public void setMoradores(List<Morador> moradores) {
+        this.moradores = moradores;
+    }
+
+}
