cfbeb69885d7bb0b69121e0f54ca00b1bdf341e0
Jeyciane Faria <jeycianedeveloper@gmail.com>
2021-10-06 05:39:14 -0300

    Modelar método executar da classe sistema

--- src/br/com/zup/Sistema.java
+++ src/br/com/zup/Sistema.java
@@ -13,9 +13,43 @@ public class Sistema {
 
     public static void menu(){
         System.out.println("\n======Bem vindo======");
-        System.out.println("\nDigite 1 - para cadastrar um imóvel.");
+        System.out.println("Digite 1 - para cadastrar um imóvel.");
+        System.out.println("Digite 2 - para exibir o cátalogo de imóveis.");
+        System.out.println("Digite 3 - para sair.");
+    }
+
+
+    public static void executar(){
+
+        Imobiliaria imobiliaria = new Imobiliaria("Imobolote");
+
+        boolean exibirMenu = true;
+
+        while(exibirMenu){
+
+            menu();
+
+            int opcaoSelecionada = capturarDados("\nDigite a opção desejada: ").nextInt();
+
+            if (opcaoSelecionada == 1){
+                //cadastrar um imóvel
+            }else if (opcaoSelecionada == 2){
+                //exibir cátalogo de imóveis
+            }else if(opcaoSelecionada == 3){
+                //sair
+                exibirMenu = false;
+                System.out.println("\nAté a proxima!");
+            }else{
+                System.out.println("\nA opção selecionada é inválida, digite novamente!");
+            }
+
+            /* System.out.println("\nDigite 1 - para cadastrar um imóvel.");
         System.out.println("\nDigite 2 - para exibir o cátalogo de imóveis.");
-        System.out.println("\nDigite 3 - para sair.");
+        System.out.println("\nDigite 3 - para sair.");*/
+        }
+
+
+
     }
 
 }
